ac24eb67d0e67f1c5c61b726b60834905fc9f878
Daniel Huang <daniel@rosetta.ai>
2018-08-06 11:22:48 +0800

    env setting

--- init.sh
+++ init.sh
@@ -3,10 +3,12 @@
 if [[ -n "$1" ]]; then
     cp -i resources/Homestead.json Homestead.json
 else
-    cp -i resources/Homestead.yaml Homestead.yaml
+    # cp -i resources/Homestead.yaml Homestead.yaml
+    cp -i Homestead.yaml.template Homestead.yaml
 fi
 
-cp -i resources/after.sh after.sh
+# cp -i resources/after.sh after.sh
+cp -i after.sh.template after.sh
 cp -i resources/aliases aliases
 
 echo "Homestead initialized!"
